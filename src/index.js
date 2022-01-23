1390b53d9fc23bb575b8e35ccc29330c3c4ed947
Twometer <twometer@outlook.de>
2022-01-24 00:13:00 +0100

    Create app

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const express = require('express')
+
+let app = express()
+app.use(express.json())
+
+const dataBaseFolder = __dirname + '../../data'
+
+app.listen(5050, () => {
+    app.get('/api/files', (req, resp) => {
+        let f = fs.readdirSync(dataBaseFolder)
+        resp.json(f)
+    })
+    app.post('/api/files', (req, resp) => {
+        let fileName = req.body.name
+        if (!fileName) return
+        if (fileName.includes('/') || fileName.includes('\\')) return
+
+        let filePath = `${dataBaseFolder}/${fileName}`
+        console.log(filePath)
+        fs.writeFileSync(filePath, req.body.content)
+        resp.json({ status: 'ok' })
+    })
+    app.use('/data', express.static(dataBaseFolder))
+    app.use('/', express.static(__dirname + '/public'))
+})
